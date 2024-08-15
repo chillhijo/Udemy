3e03f0112f258cc7564f90b2edbcca3fb7e76be8
chillhijo <pralicadusko93@gmail.com>
2024-08-15 09:35:45 +0200

    more of handling

--- tests/HandlingStaff.spec.js
+++ tests/HandlingStaff.spec.js
@@ -4,7 +4,7 @@ test('Handling validations', async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     // await page.goto("https://google.com");
     // await page.pause();
-    // await page.goBack();
+     // await page.goBack();
     // await page.goForward();
 
     //Hidden element validations
@@ -17,9 +17,11 @@ test('Handling validations', async ({ page }) => {
     await page.locator("#confirmbtn").click();
 
     //Hover handling
-    await page.pause();
     await page.locator("#mousehover").hover();
 
     //Handling iFrames
-    
+    const framePage = page.frameLocator("#courses-iframe");
+    await framePage.locator("li a[href*='lifetime-access']:visible").click();
+    const h2Text = await framePage.locator(".text h2").textContent();
+    console.log(h2Text.split(" ")[1]);
 })
